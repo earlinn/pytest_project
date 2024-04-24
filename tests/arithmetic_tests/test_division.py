987238f8ef3548bc4c0cbf861e47bfa14cca39aa
earlinn <earlinndrow@gmail.com>
2024-04-24 16:09:05 +0500

    Install pytest via poetry, add division tests

---
+++ tests/arithmetic_tests/test_division.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def division(a: int, b: int) -> int:
+    return a // b
+
+
+@pytest.mark.parametrize("a, b, result", ((10, 2, 5), (-20, 5, -4), (0, 100, 0)))
+def test_success(a, b, result):
+    assert division(a, b) == result
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        division(5, 0)
